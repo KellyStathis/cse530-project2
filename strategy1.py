976dba04d2edd99448ecc5db8fc02cedec9bc26c
Kelly Stathis <kelly.stathis@gmail.com>
2017-04-14 18:26:33 -0500

    Multiple data blocks

--- strategy1.py
+++ strategy1.py
@@ -10,8 +10,7 @@ import simpy
 import random
 
 RANDOM_SEED = 40
-#SIMULATION_TIME = 15 # not used
-#NUM_DATA_BLOCKS = 1 # need to have multiple data blocks
+NUM_DATA_BLOCKS = 2
 MAX_READ_WRITE_TIME = 200
 NUM_REQUESTS = 6
 INTERVAL_REQUESTS = 10.0  # Generate new requests roughly every x seconds
@@ -19,30 +18,30 @@ numReads = 0
 numWrites = 0
 
 env = simpy.Environment()
-def source(env, numRequests, interval, readLock, writeLock):
+def source(env, numRequests, interval, readLockArray, writeLockArray):
     """Source generates read/write requests randomly"""
     for i in range(numRequests):
-        #readWrite = 1
+        blockNum = random.randint(0, NUM_DATA_BLOCKS-1)
         readWrite = random.randint(0, 3) # 3/4 probability of read, 1/4 write
         if (readWrite!=0):
             # read block
-            c = read(env, 'Read%d' % i, readLock, writeLock, readTime=20)
+            c = read(env, 'Read%d Block%d' % (i, blockNum), readLockArray, writeLockArray, blockNum, readTime=20)
         else:
             # write to block
-            c = write(env, 'Write%d' % i, readLock, writeLock, writeTime=100)
+            c = write(env, 'Write%d Block%d' % (i, blockNum), readLockArray, writeLockArray, blockNum, writeTime=100)
         env.process(c)
         t = random.expovariate(1.0 / interval)
         yield env.timeout(t)
 
-def read(env, name, readLock, writeLock, readTime):
+def read(env, name, readLockArray, writeLockArray, blockNum, readTime):
     arrive = env.now
     print('%7.4f %s: Received' % (arrive, name))
     
     # Wait for writeLock.count to be 0
-    while writeLock.count != 0:
+    while writeLockArray[blockNum].count != 0:
         yield env.timeout(1)
         
-    with readLock.request() as req:
+    with readLockArray[blockNum].request() as req:
         yield req
         wait = env.now - arrive
         
@@ -56,15 +55,15 @@ def read(env, name, readLock, writeLock, readTime):
         yield env.timeout(timeReading)
         print('%7.4f %s: Finished' % (env.now, name))
         
-def write(env, name, readLock, writeLock, writeTime):
+def write(env, name, readLockArray, writeLockArray, blockNum, writeTime):
     arrive = env.now
     print('%7.4f %s: Received' % (arrive, name))
 
     # Wait for readLock.count to be 0
-    while readLock.count != 0:
+    while readLockArray[blockNum].count != 0:
         yield env.timeout(1)
         
-    with writeLock.request() as req:
+    with writeLockArray[blockNum].request() as req:
         # Wait for the block to be free from reading or writing
         yield req
         wait = env.now - arrive
@@ -87,7 +86,11 @@ random.seed(RANDOM_SEED)
 env = simpy.Environment()
 
 # Start processes and run
-readLock = simpy.Resource(env, capacity=5)
-writeLock = simpy.Resource(env, capacity=1)
-env.process(source(env, NUM_REQUESTS, INTERVAL_REQUESTS, readLock, writeLock))
+readLockArray = []
+writeLockArray = []
+for i in range (0,NUM_DATA_BLOCKS):
+    readLockArray.append(simpy.Resource(env, capacity=5))
+    writeLockArray.append(simpy.Resource(env, capacity=1))
+    
+env.process(source(env, NUM_REQUESTS, INTERVAL_REQUESTS, readLockArray, writeLockArray))
 env.run()
